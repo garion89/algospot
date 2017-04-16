24d8780bb5fa702e1c47c26e3f25e5e51b336e7d
garion89 <garion89@snu.ac.kr>
2017-04-16 15:22:03 +0900

    solved!

--- src/QUADTREE.java
+++ src/QUADTREE.java
@@ -20,6 +20,51 @@ public class QUADTREE {
     static String code;
     static Node root;
 
+    static class Node {
+        //type consists of 'x', 'b', 'w';
+        char type;
+        //0 means west north, 1 means east north, 2 means west south, 3 means east south
+        Node[] children;
+
+        public Node(char type) {
+            this.type = type;
+        }
+
+        public Node(char type, Node[] children) {
+            this.type = type;
+            this.children = children;
+        }
+
+        void upsideDown() {
+            Node temp;
+
+            if(type == 'x') {
+                temp = children[0];
+                children[0] = children[2];
+                children[2] = temp;
+
+                temp = children[1];
+                children[1] = children[3];
+                children[3] = temp;
+
+                for(int i = 0; i < 4; i++) {
+                    children[i].upsideDown();
+                }
+            }
+        }
+
+        void print() {
+            if(type == 'x') {
+                System.out.print(type);
+                for(int i = 0; i < 4; i++) {
+                    children[i].print();
+                }
+            } else {
+                System.out.print(type);
+            }
+        }
+    }
+
     public static Node parser() {
         curr = code.charAt(count);
         count++;
@@ -51,51 +96,6 @@ public class QUADTREE {
             root.upsideDown();
             root.print();
             System.out.println();
-        }
-    }
-}
-
-class Node {
-    //type consists of 'x', 'b', 'w';
-    char type;
-    //0 means west north, 1 means east north, 2 means west south, 3 means east south
-    Node[] children;
-
-    public Node(char type) {
-        this.type = type;
-    }
-
-    public Node(char type, Node[] children) {
-        this.type = type;
-        this.children = children;
-    }
-
-    void upsideDown() {
-        Node temp;
-
-        if(type == 'x') {
-            temp = children[0];
-            children[0] = children[2];
-            children[2] = temp;
-
-            temp = children[1];
-            children[1] = children[3];
-            children[3] = temp;
-
-            for(int i = 0; i < 4; i++) {
-                children[i].upsideDown();
-            }
-        }
-    }
-
-    void print() {
-        if(type == 'x') {
-            System.out.print(type);
-            for(int i = 0; i < 4; i++) {
-                children[i].print();
-            }
-        } else {
-            System.out.print(type);
         }
     }
 }
